d5f1ca2b7695c5d9809749ae9e67a4edcb0d01ba
pramod729 <aryalpramod22@gmail.com>
2025-12-23 13:39:44 +0545

    Fix Firebase error

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ import 'package:ayojana_hub/vendor_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+
 
 // Screens
 
@@ -20,7 +22,9 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+     options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AyojanaHubApp());
 }
 
